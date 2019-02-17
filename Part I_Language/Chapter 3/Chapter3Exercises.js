ca0e9227d79af1d89ca0eeac2d2bbe69322ed6fc
X-ManStan <d-hitze@comcast.net>
2019-02-17 10:19:30 -0600

    updated format

--- Part I_Language/Chapter 3/Chapter3Exercises.js
+++ Part I_Language/Chapter 3/Chapter3Exercises.js
@@ -7,14 +7,10 @@ function mathMin(a, b){
 mathMin(2, 30); // --> 2
 
 // same end result, using arrow function instead ----------------
-mathMin = (a, b) => {return Math.min(a, b)};
+const mathMin = (a, b) => {return Math.min(a, b)};
 
 mathMin(2, 30); // --> 2
 
 // 2) 0 = even, 1 is odd, any other num(n), its eveness is the same as n-2
-const isEven = (n) => {
-    const count = (current, history) => {
-        if()
-    }
-}
-// 3)
+
+// 3) 
