ca40ad05209f330eb0b4494c143e05fdfb46153c
X-ManStan <d-hitze@comcast.net>
2019-02-17 00:18:57 -0600

    Completed question 1

---
+++ Part I_Language/Chapter 3/Chapter3Exercises.js
@@ -0,0 +1,9 @@
+// Chapter 3 Exercises on page 56
+// 1) create function that use Math.min and finds the lowest number
+function mathMin(a, b){
+	return Math.min(a, b);
+}
+
+mathMin(2, 30); // --> 2
+
+// 2)
