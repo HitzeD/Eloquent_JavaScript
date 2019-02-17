b9471fb2ec48ca220a6ab1dab8177360b40ec3d8
X-ManStan <d-hitze@comcast.net>
2019-02-16 23:21:43 -0600

    initial upload

---
+++ Part I_Language/Chapter 2/Chapter2Exercise.js
@@ -0,0 +1,67 @@
+// 1) exercise on pg 37.
+
+
+
+// function triangle(num){
+//     let hash = '#';
+//     for(i = 0; i >= num; i++){
+//         hash.repeat(i);
+//         console.log(hash);
+//     };
+//     console.log(hash);
+// }
+
+// triangle(7);
+
+// This is the correct method!!! exercise on pg 37. ---------------------
+function forLoops(){
+    let hash = '#';
+    for(let i = 0; i <= 7; i++){
+        console.log(hash.repeat(i));
+    };
+};
+
+// 2)  this is correct!
+
+function fizzBuzz(){
+    for(i = 0; i <= 100; i++){
+		if((i % 3 === 0) && (i % 5 === 0)){
+			console.log('FizzBuzz')
+		} else if (i % 3 === 0){
+            console.log('Fizz');
+        } else if(i % 5 === 0){
+            console.log('Buzz')
+        } else {
+            console.log(i);
+        }
+    };
+}
+
+fizzBuzz();
+
+// 3) this is correct!
+
+function chessBoard(num) {
+	let size = num / 2;
+	const on = " # " + "   ";
+	const off = "   " + " # ";
+	const newLine = '\n';
+	let reon = on.repeat(size), reoff = off.repeat(size);
+	const both = reon + newLine + reoff + newLine;
+	console.log(both.repeat(size));
+}
+chessBoard(16);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
