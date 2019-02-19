ec45fa86848ab392ab1be6649e03112350078298
X-ManStan <d-hitze@comcast.net>
2019-02-19 14:21:16 -0600

    answered 2 and started 3

--- Part I_Language/Chapter 3/Chapter3Exercises.js
+++ Part I_Language/Chapter 3/Chapter3Exercises.js
@@ -11,6 +11,42 @@ const mathMin = (a, b) => {return Math.min(a, b)};
 
 mathMin(2, 30); // --> 2
 
-// 2) 0 = even, 1 is odd, any other num(n), its eveness is the same as n-2
+// 2)
+
+function isEven(n) {
+    if(n == 0){
+        return true;
+    } else if(n == 1){
+        return false;
+    } else if(n < 0){
+        return isEven(-n);
+    } else{
+        return isEven(n - 2);
+    }
+  }
+
+console.log(isEven(50));
+console.log(isEven(75));
+console.log(isEven(-1));
+
 
 // 3) 
+
+function countChar(str, char){
+    // let count = 0;
+    for(let i = 0; i < str.length; i++){
+        let count = 0;
+        if(i == char){
+            count++;           
+        };
+        return count;
+    };
+}
+
+  console.log(countChar("kakkerlak", "k"));
+  
+  console.log(countBs("BBC"));
+  // → 2
+  console.log(countChar("kakkerlak", "k"));
+  // → 4
+
